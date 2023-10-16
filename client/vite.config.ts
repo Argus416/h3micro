54569cd663e4562f5784b33bd9edb39ef61f9287
Mohamad <alkhatib.m804@gmail.com>
2023-10-16 17:10:44 +0200

    Fix client hotreload

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,14 +5,14 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
 	server: {
 		host: '0.0.0.0',
+		strictPort: true,
+		port: 5173,
 		watch: {
 			usePolling: true,
 		},
-		strictPort: true,
-		port: 5173,
 		hmr: {
 			host: '0.0.0.0',
-			port: 3010,
+			port: 5174,
 		},
 	},
 	plugins: [react()],
