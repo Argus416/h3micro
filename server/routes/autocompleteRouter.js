5b001149230d8d9aa6e925ce289938db862dbc94
Mohamad AL KHATIB <alkhatib.m804@gmail.com>
2024-01-11 17:05:50 +0100

    create autocomplete router

---
+++ server/routes/autocompleteRouter.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const router = express.Router();
+const { client } = require('../db/elastic');
+const { logger } = require('../config/logger');
+
+/**
+ * @openapi
+ * /autocomplete/users/{query}:
+ *   get:
+ *     summary: Get autocomplete suggestions for user names
+ *     tags:
+ *       - Autocomplete
+ *     parameters:
+ *       - in: path
+ *         name: query
+ *         required: true
+ *         description: The prefix to use for autocomplete suggestions
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Successful response with autocomplete suggestions
+ *         content:
+ *           application/json:
+ *             example:
+ *               suggestions:
+ *                 - John Doe
+ *                 - John Smith
+ *                 - ...
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             example:
+ *               error: Autocompletion error
+ */
+
+router.get('/users/:query', async (req, res) => {
+	const { query } = req.params;
+
+	try {
+		const data = await client.search({
+			index: 'users', // Replace 'users' with your Elasticsearch index
+			body: {
+				suggest: {
+					userSuggest: {
+						prefix: query,
+						completion: {
+							field: 'name.completion',
+							size: 5, // Number of suggestions to return
+						},
+					},
+				},
+			},
+		});
+
+		// const suggestions = data.suggest.userSuggest[0]?.options.map(
+		// 	(option) => option._source.name
+		// );
+
+		res.json({ data });
+	} catch (e) {
+		logger.error('Autocompletion error', e);
+		res.send(`Autocompletion error: ${e}`);
+	}
+});
+
+module.exports = router;
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const { Post } = require('../db');
 const postRouter = require('./postRouter');
 const userRouter = require('./userRouter');
+const autocompleteRouter = require('./autocompleteRouter');
 
 router.use('/posts', postRouter);
 router.use('/users', userRouter);
+router.use('/autocomplete', autocompleteRouter);
 
 router.get('/', (req, res) => {
 	res.send('Server is running');
